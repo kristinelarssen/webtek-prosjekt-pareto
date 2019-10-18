5b4e519a53e2b8a7d70d856325ff7af96946584f
Barfrost <51021383+Barfrost@users.noreply.github.com>
2019-10-19 00:17:09 +0200

    Opprettet index.html for å starte testing av createModElement()

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,8 +1,4 @@
-function createNavbar() {
-
-}
-
-function createNavbarElement() {
+function createNavbar(tarElement) {
 
 }
 
@@ -10,6 +6,28 @@ function createFooter() {
 
 }
 
-function createFooterElement() {
 
+// eleattr = array på formen
+function createModElement(tagName, content, tar, url = "", eleattr = "") {
+    const node = document.createElement(tagName);
+    const textnode = document.createTextNode(content);
+    node.appendChild(textnode);
+    if (url !== "") {
+        node.setattribute("href", url);
+    }
+    if (Array.isArray(eleattr)) {
+        if (Array.isArray(eleattr[0])) {
+            for (i = 0; i < eleattr.length; i++) {
+                node.setAttribute(eleattr[i][0],eleattr[i][1]);
+            }
+        }
+        else {
+            node.setAttribute(eleattr[0],eleattr[1]);
+        }
+    }
+    tar.appendChild(node);
 }
+
+//test
+const headertarget = document.querySelector("#js-header");
+createModElement("p", "Hei dette er en test", headertarget, "", [["class", "box"],["required",""]])
