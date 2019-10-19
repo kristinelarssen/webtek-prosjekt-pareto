da937a31956c86d782a5fa4ac114ba83e32b3302
Barfrost <51021383+Barfrost@users.noreply.github.com>
2019-10-19 16:18:47 +0200

    Endret img styling i css til en klasse som er blitt satt inn i alle bilder i P1 og P2, slik at vi ikke må jobbe rundt disse i P3

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -4,14 +4,14 @@ function createNavbar(tarElement) {
     const navbar = document.querySelector("#js-navbar");
     createModElement("a",navbar,"","index.html",["id","js-header-logo"])
     createModElement("img",document.querySelector("#js-header-logo"), "", "", [["src", "img/pareto_logo.png"], ["id", "js-header-logo"], ["alt", "Pareto-logo"]]);
-    createModElement("ul", navbar, "", "", ["id", "js-navbar-ul"]);
+    createModElement("ul", navbar, "", "", ["id", "js-header-ul"]);
 
     for (i = 0; i < 6; i++) {
-        createModElement("li", navbar, "", "", ["class", "js-header-li"]) 
+        createModElement("li", document.querySelector("#js-header-ul"), "", "", ["class", "js-header-li"]) 
     }
     const lielements = document.getElementsByClassName("js-header-li")
-    createModElement("a", lielements[0], "Om Pareto", "om_pareto.html", ["class", "js-header-link"]);
-    createModElement("a", lielements[1], "Arrangmenter", "arrangementer.html", ["class", "js-header-link"]);
+    createModElement("a", lielements[0], "Om Pareto", "om_pareto.html", [["class", "js-header-link"],"id","js-dropdown"]);
+    createModElement("a", lielements[1], "Arrangementer", "arrangementer.html", ["class", "js-header-link"]);
     createModElement("a", lielements[2], "Ny student?", "ny_student.html", ["class", "js-header-link"]);
     createModElement("a", lielements[3], "Bli medlem", "bli_medlem.html", ["class", "js-header-link"]);
     createModElement("a", lielements[4], "Kontakt oss", "kontakt.html", ["class", "js-header-link"]);
