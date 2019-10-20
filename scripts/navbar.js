8c47a27718b1b7c76850219584fbbed10d16f7b5
Barfrost <51021383+Barfrost@users.noreply.github.com>
2019-10-20 12:55:02 +0200

    Lagt til divs for de ulike seksjonene i footer.

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,17 +2,18 @@ function createNavbar(tarElement) {
     createModElement("nav", tarElement, "", "", ["id", "js-navbar"]);
 
     const navbar = document.querySelector("#js-navbar");
-    createModElement("a",navbar,"","index.html",["id","js-header-logo"]);
+    createModElement("a", navbar, "", "index.html", ["id", "js-header-logo"]);
 
     const logo = document.querySelector("#js-header-logo");
-    createModElement("img",logo, "", "", [["src", "img/pareto_logo.png"], ["id", "js-header-logoimg"], ["alt", "Pareto-logo"]]);
+    createModElement("img", logo, "", "", [["src", "img/pareto_logo.png"], ["id", "js-header-logoimg"], ["alt", "Pareto-logo"]]);
     createModElement("ul", navbar, "", "", ["id", "js-header-ul"]);
 
     for (i = 0; i < 6; i++) {
         createModElement("li", document.querySelector("#js-header-ul"), "", "", ["class", "js-header-li"]);
     }
-    const lielements = document.getElementsByClassName("js-header-li")
-    createModElement("a", lielements[0], "Om Pareto", "om_pareto.html", [["class", "js-header-link"],"id","js-dropdown"]);
+
+    const lielements = document.getElementsByClassName("js-header-li");
+    createModElement("a", lielements[0], "Om Pareto", "om_pareto.html", [["class", "js-header-link"], "id", "js-dropdown"]);
     createModElement("a", lielements[1], "Arrangementer", "arrangementer.html", ["class", "js-header-link"]);
     createModElement("a", lielements[2], "Ny student?", "ny_student.html", ["class", "js-header-link"]);
     createModElement("a", lielements[3], "Bli medlem", "bli_medlem.html", ["class", "js-header-link"]);
@@ -21,18 +22,17 @@ function createNavbar(tarElement) {
 }
 
 function createFooter(tarElement) {
-    createModElement("div",tarElement,"","",[["id","js-footer-divikon"],["class","js-footer-div"]]);
-    createModElement("div",tarElement,"","",[["id","js-footer-divkontakt"],["class","js-footer-div"]]);
-    createModElement("div",tarElement,"","",[["id","js-footer-divadresse"],["class","js-footer-div"]]);
+    createModElement("div", tarElement, "", "", [["id", "js-footer-divikon"], ["class", "js-footer-div"]]);
+    createModElement("div", tarElement, "", "", [["id", "js-footer-divkontakt"], ["class", "js-footer-div"]]);
+    createModElement("div", tarElement, "", "", [["id", "js-footer-divadresse"], ["class", "js-footer-div"]]);
 
     const divikon = document.querySelector("#js-footer-divikon");
-    const divkontakt = document.querySelector("#js-footer-divikon");
-    const divadresse = document.querySelector("#js-footer-divikon");
+    const divkontakt = document.querySelector("#js-footer-divkontakt");
+    const divadresse = document.querySelector("#js-footer-divadresse");
 
-    createModElement("ul", divikon,"","",["id","ulikon"])
-    createModElement("ul",divkontakt,"","",["id","ulkontakt"])
-    createModElement("ul",divadresse,"","",["id","uladresse"])
-    
+    createModElement("ul", divikon, "", "", ["id", "ulikon"]);
+    createModElement("ul", divkontakt, "", "", ["id", "ulkontakt"]);
+    createModElement("ul", divadresse, "", "", ["id", "uladresse"]);
 }
 
 
