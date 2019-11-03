c1fdb4502298234053b337734247c3c118d9646e
BjørnA <51021383+Barfrost@users.noreply.github.com>
2019-11-03 02:22:53 +0100

    Juster footer, lagt til fil for script til validering

---
+++ scripts/skjemavalidering.js
@@ -0,0 +1,8 @@
+/* Something something */
+
+const kontaktNavn = document.getElementById("kontaktOssNavnInput");
+const kontaktEpost = document.getElementById("kontaktOssEpostInput");
+const kontaktEmne = document.getElementById("kontaktOssEmneInput");
+const kontaktMelding = document.getElementById("kontaktOssMeldingInput");
+const kontaktSubmit = document.querySelector("form > button");
+
