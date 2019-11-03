a48a8a25160c62523860329ffaec9f8b07306097
Barfrost <51021383+Barfrost@users.noreply.github.com>
2019-11-03 18:40:05 +0100

    Siste

--- scripts/skjemavalidering.js
+++
@@ -1,8 +0,0 @@
-/* Something something */
-
-const kontaktNavn = document.getElementById("kontaktOssNavnInput");
-const kontaktEpost = document.getElementById("kontaktOssEpostInput");
-const kontaktEmne = document.getElementById("kontaktOssEmneInput");
-const kontaktMelding = document.getElementById("kontaktOssMeldingInput");
-const kontaktSubmit = document.querySelector("form > button");
-
