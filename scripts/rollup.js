00b6f557ee5e14316b279e967daec131c20ea358
Frida <55274986+fridabjorn@users.noreply.github.com>
2019-11-05 11:21:29 +0100

    fikset knappen, igjen

--- scripts/rollup.js
+++ scripts/rollup.js
@@ -4,10 +4,10 @@ scrollupbutton = document.getElementById("js-scrollup");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
     scrollupbutton.style.display = "block";
   } else {
-    scrollupbutton.style.display = "none";
+        scrollupbutton.style.display = "none";
   }
 }
 
