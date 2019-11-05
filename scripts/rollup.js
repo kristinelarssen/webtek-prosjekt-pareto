e8696cd9359dde67a6e5a3075d96d33852513a13
Frida <55274986+fridabjorn@users.noreply.github.com>
2019-11-05 11:27:28 +0100

    fiksa

--- scripts/rollup.js
+++ scripts/rollup.js
@@ -4,7 +4,7 @@ scrollupbutton = document.getElementById("js-scrollup");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     scrollupbutton.style.display = "block";
   } else {
         scrollupbutton.style.display = "none";
