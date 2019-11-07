b668609585fdaf2e062be76311de2431a79f539b
Ingrid Gomo <54813201+IngridGomo@users.noreply.github.com>
2019-11-07 21:22:06 +0100

    konfetti, beskjed, changes

--- scripts/bli_medlem.js
+++ scripts/bli_medlem.js
@@ -15,12 +15,22 @@ email1.addEventListener('input', validateEmails)
 email2.addEventListener('input', validateEmails)
 
 
-/* Legger til et medlem npr man trykker på send inn */
-let button = document.getElementById('medlem_button')
+
+/* Legger til et medlem når man trykker på send inn */
+let send_inn_knapp = document.getElementById('medlem_button')
 
 function addMedlem() {
-  let medlemsnummer = document.getElementById('om_oss_medlemsnummer')
-  medlemsnummer.innerHTML = "321";
+  let medlemsnummer = document.getElementById('om_oss_medlemsnummer').innerText;
+  medlemsnummer.innerText = "321";
 }
 
-button.addEventListener('click',addMedlem)
+send_inn_knapp.addEventListener('click',addMedlem)
+
+
+
+/* Mottaksbeskjed */
+const popup = document.getElementById('medlem_innsending_mottatt')
+
+function changeDisplay() {
+  popup.style.display = 'block'
+}
